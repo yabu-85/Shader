292f57e53f476f310b80ace43628b84bfc13be12
海斗 矢吹 <220177@jc-21.jp>
2023-11-17 11:23:12 +0900

    モデルの表示までやった

--- Stage.cpp
+++ Stage.cpp
@@ -4,6 +4,9 @@
 
 namespace {
     Sprite* img = nullptr;
+
+    int hModel[2] = { -1, -1 };
+
 }
 
 //コンストラクタ
@@ -24,6 +27,11 @@ void Stage::Initialize()
     hModel_ = Model::Load("Assets/numberCube.fbx");
     assert(hModel_ >= 0);
 
+    hModel[0] = Model::Load("Assets/ground.fbx");
+    assert(hModel[0] >= 0);
+    hModel[1] = Model::Load("Assets/arrow.fbx");
+    assert(hModel[1] >= 0);
+
     img = new Sprite();
     img->Initialize("neko.png");
 }
@@ -37,12 +45,28 @@ void Stage::Update()
 void Stage::Draw()
 {
     Transform blockTrans;
-
+    blockTrans.position_ = XMFLOAT3(0.0f, 0.8f, 0.0f);
     Model::SetTransform(hModel_, blockTrans);
     Model::Draw(hModel_);
 
+    Transform groundTrans;
+    groundTrans.scale_ = XMFLOAT3(6.0f, 6.0f, 6.0f);
+    Model::SetTransform(hModel[0], groundTrans);
+    Model::Draw(hModel[0]);
+
+    XMFLOAT3 arrowRotate[3] = { XMFLOAT3(90.0f, 0.0f, 0.0f), XMFLOAT3(0.0f, 0.0f, 0.0f), XMFLOAT3(0.0f, 0.0f, 90.0f) };
+    for (int i = 0; i < 3; i++) {
+        Transform arrowTrans;
+        arrowTrans.rotate_ = arrowRotate[i];
+        arrowTrans.scale_ = XMFLOAT3(0.2f, 0.2f, 0.2f);
+        Model::SetTransform(hModel[1], arrowTrans);
+        Model::Draw(hModel[1]);
+    }
+    
     blockTrans.scale_ = XMFLOAT3(1.5f, 1.5f, 1.5f);
     //img->Draw(blockTrans);
+
+    
 }
 
 //開放
