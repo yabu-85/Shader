e58a4f98cc73a02cf35d38715cb2fb15699ec8bb
海斗 矢吹 <220177@jc-21.jp>
2024-01-12 11:22:48 +0900

    OutlineShaderのいらないやつ消した

--- OutLine.hlsl
+++ OutLine.hlsl
@@ -1,29 +1,10 @@
 //───────────────────────────────────────
-// テクスチャ＆サンプラーデータのグローバル変数定義
-//───────────────────────────────────────
-Texture2D	g_texture : register(t0);		//テクスチャー
-SamplerState	g_sampler : register(s0);	//サンプラー
-
-//───────────────────────────────────────
 // コンスタントバッファ
 // DirectX 側から送信されてくる、ポリゴン頂点以外の諸情報の定義
 //───────────────────────────────────────
 cbuffer global : register(b0)
 {
 	float4x4	g_matWVP;			// ワールド・ビュー・プロジェクションの合成行列
-	float4x4	g_matW;				// ワールド行列
-	float4x4	g_matNormal;		// 
-	float4		g_diffuseColor;		// ディフューズカラー（マテリアルの色）
-	float4		g_ambientColor;		// 環境光の色
-	float4		g_specular;			// 鏡面反射光の色
-	float		g_shuniness;		// 鏡面反射光の強さ
-	bool		g_isTexture;		// テクスチャ貼ってあるかどうか
-};
-
-cbuffer light : register(b1) 
-{
-	float4		g_lightPosition;	// ライトの向き
-	float4		g_eyePosition;		// カメラの向き
 };
 
 //───────────────────────────────────────
@@ -32,10 +13,6 @@ cbuffer light : register(b1)
 struct VS_OUT
 {
 	float4 pos    : SV_POSITION;	//位置
-	float2 uv	  : TEXCOORD2;		//uv座標
-	float4 color  : COLOR;			//色（明るさ）
-	float4 eye	  : TEXCOORD1;		//視線
-	float4 normal : TEXCOORD0;		//法線
 };
 
 //───────────────────────────────────────
@@ -48,9 +25,6 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 
 	//法線を回転
 	normal.w = 0;
-	normal = mul(normal, abs(g_matNormal));
-	normal = normalize(normal);
-	outData.normal = normal;
 
 	//ローカル座標に、ワールド・ビュー・プロジェクション行列をかけて
 	//スクリーン座標に変換し、ピクセルシェーダーへ
